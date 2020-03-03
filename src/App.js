ecb3ae3702eda38c6a7b500acfc42eda808439f3
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2020-03-03 09:12:49 +0000

    added header and searchbox with out function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 // import './App.css';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox'
 
 // Just importet infof !!!
 
 function App() {
     return (
         <div className="tc ">
+            <h1>Welcome to RoboFriends </h1>
+            <SearchBox />
             <CardList robots={robots} number={1}/>
             <CardList robots={robots} number={2}/>
         </div>
---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div className='ma2 pa2'>
+            <input type='search' placeholder='search robots' />
+        </div>
+    );
+}
+
+export default SearchBox;
