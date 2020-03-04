962fa7297e544d115c156c828a977bd6b2f0fb3c
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2020-03-04 23:07:06 +0000

    Updates

---
+++ precache-manifest.950aa1ab5a84489dd1f35ce5bb2a1b5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e432e71b57634e1e9cca33b0da6ea1",
+    "url": "/memo/index.html"
+  },
+  {
+    "revision": "d60a1dab6f2a73c16c94",
+    "url": "/memo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a608dba5a2902da1b81d",
+    "url": "/memo/static/css/main.9e9ebfce.chunk.css"
+  },
+  {
+    "revision": "d60a1dab6f2a73c16c94",
+    "url": "/memo/static/js/2.6cfa96fa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/memo/static/js/2.6cfa96fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a608dba5a2902da1b81d",
+    "url": "/memo/static/js/main.538c14ee.chunk.js"
+  },
+  {
+    "revision": "1d53ec4be965ebb12159",
+    "url": "/memo/static/js/runtime-main.a3041411.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/memo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
